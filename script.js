bcfc03bc3f59182c994fa2cb43bfcc7e20dd9e55
jeanlclucas <carvalho.jeanlucas@gmail.com>
2025-11-30 22:40:08 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ document.addEventListener("DOMContentLoaded", () => {
   const menuToggle = document.getElementById("menu-toggle");
   const navbar = document.querySelector(".navbar");
 
-  menuToggle.addEventListener("click", () => {
-    navbar.classList.toggle("active");
-  });
+  if (menuToggle && navbar) {
+    menuToggle.addEventListener("click", () => {
+      navbar.classList.toggle("active");
+    });
+  }
 });
