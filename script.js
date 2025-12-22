f6e1f5b543417747aaac1de571850464b79582fe
jeanlclucas <carvalho.jeanlucas@gmail.com>
2025-12-22 00:02:42 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ document.addEventListener("DOMContentLoaded", () => {
   const menuToggle = document.getElementById("menu-toggle");
   const navbar = document.querySelector(".navbar");
 
-  // Só ativa o menu hambúrguer se existir no index
   if (menuToggle && navbar) {
     menuToggle.addEventListener("click", () => {
       navbar.classList.toggle("active");
     });
   }
 });
+
